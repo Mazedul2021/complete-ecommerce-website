d359d2c8cacfff99b7adbf05c5a538145b69d640
Mazedul2021 <mdmazedul0172@gmail.com>
2026-01-29 00:42:24 +0600

    sort button

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -13,7 +13,7 @@ const ProductItem = (props) => {
     <div className='productItem shadow-lg rounded-md overflow-hidden border-1 border-[rgba(0,0,0,0.1)]'>
         <div className='group imgWrapper w-[100%] overflow-hidden rounded-md rounded-bl-none rounded-br-none relative'>
             <img src={Product1} className='w-full group-hover:scale-[1.1] transition-all duration-200'/>
-            <span class="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-primary text-white rounded-lg p-1 text-[12px] font-[500]">14%</span>
+            <span className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-primary text-white rounded-lg p-1 text-[12px] font-[500]">14%</span>
             <div className='action flex items-center top-[-200px] right-[5px] z-50 flex-col w-[50px] absolute gap-2 group-hover:top-[15px] opacity-0 group-hover:opacity-100 transition-all duration-300'>
               <Button className='!w-[35px] !h-[35px] !min-w-[35px] !bg-white !rounded-full hover:!bg-primary hover:!text-white group'>
                 <MdOutlineZoomOutMap className='!text-[18px]'/>
@@ -24,12 +24,12 @@ const ProductItem = (props) => {
             </div>
         </div>
         <div className='info p-3'>
-          <h6 class="text-[13px] !font-[400]"><Link class="link transition-all">Flying Machine</Link></h6>
+          <h6 className="text-[13px] !font-[400]"><Link className="link transition-all">Flying Machine</Link></h6>
         <h3 className='text-[12px] lg:text-[13px] title mt-1 font-[500] mb- text-[#000]'><Link to='/' className='link'>NAVIFORCE 9214 Analogue Luxury Men Watch</Link></h3>
         <Rating name="size-small" defaultValue={4} size="small" readOnly/>
-        <div class="flex items-center gap-4 justify-between">
-          <span class="oldPrice line-through text-gray-500 text-[12px] lg:text-[14px] font-[500]">₹2,250.00</span>
-          <span class="price text-primary text-[12px] lg:text-[14px]  font-[600]">₹1,999.00</span></div>
+        <div className="flex items-center gap-4 justify-between">
+          <span className="oldPrice line-through text-gray-500 text-[12px] lg:text-[14px] font-[500]">৳2,250.00</span>
+          <span className="price text-primary text-[12px] lg:text-[14px]  font-[600]">৳1,999.00</span></div>
         </div>
     </div>
   )
--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -4,9 +4,19 @@ import ProductItem from "../ProductItem/ProductItem";
 import { IoGrid } from "react-icons/io5";
 import Button from "@mui/material/Button";
 import { FaList } from "react-icons/fa";
-
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const ProductListing = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <section className="py-5">
       <div className="bg-white p-2 mt-4">
@@ -15,14 +25,49 @@ const ProductListing = () => {
             <Sidebar />
           </div>
           <div className="rightContent w-full lg:w-[80%] py-3 px-2">
-
             <div className="bg-[#f1f1f1] p-2 w-full mb-3 flex items-center justify-between rounded-md">
               <div className="col1 flex items-center">
-                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full"><IoGrid className="!text-[rgba(0,0,0,0.7)] text-[14px]"/></Button>
-                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full"><FaList className="!text-[rgba(0,0,0,0.7)] text-[14px]"/></Button>
-                <span className="text-[14px] hidden sm:block md:block lg:block font-[500] pl-3 text-[rgba(0,0,0,0.7)]">There are 10 products.</span>
-                </div>
-
+                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full">
+                  <IoGrid className="!text-[rgba(0,0,0,0.7)] text-[14px]" />
+                </Button>
+                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full">
+                  <FaList className="!text-[rgba(0,0,0,0.7)] text-[14px]" />
+                </Button>
+                <span className="text-[14px] hidden sm:block md:block lg:block font-[500] pl-3 text-[rgba(0,0,0,0.7)]">
+                  There are 10 products.
+                </span>
+              </div>
+              <div className="col2 ml-auto flex items-center justify-end gap-3 pr-4">
+                <span className="text-[14px] font-[500] pl-3 text-[rgba(0,0,0,0.7)]">
+                  Sort By
+                </span>
+                <Button
+                  id="basic-button"
+                  aria-controls={open ? "basic-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                  className="!bg-white !text-[12px] !text-[#000] !capitalize !border-2 !border-[#000]"
+                >
+                  Sort by latest
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  slotProps={{
+                    list: {
+                      "aria-labelledby": "basic-button",
+                    },
+                  }}
+                >
+                  <MenuItem onClick={handleClose}>Sort by latest</MenuItem>
+                  <MenuItem onClick={handleClose}>sort by name: A to Z</MenuItem>
+                  <MenuItem onClick={handleClose}>sort by price: low to high</MenuItem>
+                  <MenuItem onClick={handleClose}>sort by price: high to low</MenuItem>
+                </Menu>
+              </div>
             </div>
 
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
