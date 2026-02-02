aceb43a5ee61fb3f408d6180c266d618d3337093
Mazedul2021 <mdmazedul0172@gmail.com>
2026-02-03 01:31:15 +0600

    Shop page done

--- src/components/ProductItemListView/ProductItemListView.jsx
+++ src/components/ProductItemListView/ProductItemListView.jsx
@@ -6,17 +6,19 @@ import { FaRegHeart } from "react-icons/fa";
 import { MdOutlineZoomOutMap } from "react-icons/md";
 import Button from '@mui/material/Button';
 import Product1 from "../../assets/Image/product/1.webp"
+import { FaCartPlus } from "react-icons/fa";
 
 
-const ProductItem = (props) => {
+
+const ProductItemListView = (props) => {
   return (
-    <div className='productItem shadow-lg rounded-md overflow-hidden border-1 border-[rgba(0,0,0,0.1)]'>
-        <div className='group imgWrapper w-[100%] overflow-hidden rounded-md rounded-bl-none rounded-br-none relative'>
-          <Link to="/">
-            <div className='img h-[220px] overflow-hidden'>
-            <img src={Product1} className='w-full group-hover:scale-[1.1] transition-all duration-200'/>
-            </div>
-          </Link>
+    <div className='productItem p-4 shadow-md hover:shadow-lg hover:scale-[1.02] transition-all duration-200 rounded-md overflow-hidden border-1 border-[rgba(0,0,0,0.1)] flex items-center flex-col lg:flex-row'>
+        <div className='group imgWrapper w-full lg:w-[25%]  overflow-hidden  rounded-md relative'>
+            <Link to="/">
+              <div className='img  overflow-hidden'>
+                <img src={Product1} className='w-full group-hover:scale-[1.1] transition-all duration-200'/>
+              </div>
+            </Link>
             <span className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-primary text-white rounded-lg p-1 text-[12px] font-[500]">14%</span>
             <div className='action flex items-center top-[-200px] right-[5px] z-50 flex-col w-[50px] absolute gap-2 group-hover:top-[15px] opacity-0 group-hover:opacity-100 transition-all duration-300'>
               <Button className='!w-[35px] !h-[35px] !min-w-[35px] !bg-white !rounded-full hover:!bg-primary hover:!text-white group'>
@@ -27,16 +29,20 @@ const ProductItem = (props) => {
               </Button>
             </div>
         </div>
-        <div className='info p-3'>
+        <div className='info p-3 py-3 w-[75%]'>
           <h6 className="text-[13px] !font-[400]"><Link className="link transition-all">Flying Machine</Link></h6>
-        <h3 className='text-[12px] lg:text-[13px] title mt-1 font-[500] mb- text-[#000]'><Link to='/' className='link'>NAVIFORCE 9214 Analogue Luxury Men Watch</Link></h3>
+        <h3 className='text-[12px] lg:text-[18px] title mt-3 mb-3 font-[500] text-[#000]'><Link to='/' className='link'>NAVIFORCE 9214 Analogue Luxury Men Watch</Link></h3>
+        <p className='text-[14px] mb-3'>Lorem Ipsum is simply dummy text of the printing and typessdfsdfsdfsdfetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         <Rating name="size-small" defaultValue={4} size="small" readOnly/>
-        <div className="flex items-center gap-4 justify-between">
+        <div className="flex items-center gap-4 ">
           <span className="oldPrice line-through text-gray-500 text-[12px] lg:text-[14px] font-[500]">৳2,250.00</span>
           <span className="price text-primary text-[12px] lg:text-[14px]  font-[600]">৳1,999.00</span></div>
+          <div className='mt-3'>
+            <Button className='btn-org gap-2'><FaCartPlus className='text-[18px]' /> Add to Cart</Button>
+          </div>
         </div>
     </div>
   )
 }
 
-export default ProductItem
+export default ProductItemListView
--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import ProductItem from "../ProductItem/ProductItem";
+import ProductItemListView from "../ProductItemListView/ProductItemListView"
 import { IoGrid } from "react-icons/io5";
 import Button from "@mui/material/Button";
 import { FaList } from "react-icons/fa";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Pagination from '@mui/material/Pagination';
 
 const ProductListing = () => {
+  const [itemView, setItemView] = useState('grid')
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -18,7 +21,7 @@ const ProductListing = () => {
   };
 
   return (
-    <section className="py-5">
+    <section className="py-5 pb-0">
       <div className="bg-white p-2 mt-4">
         <div className="container flex gap-3">
           <div className="sidebarWrapper fixed-bottom-[100%] left-0 w-full lg:h-full lg:static lg:w-[20%] bg-white z-[102] lg:z-[100] p-3 lg:p-0  transition-all lg:opacity-100 opacity-0 ">
@@ -26,11 +29,11 @@ const ProductListing = () => {
           </div>
           <div className="rightContent w-full lg:w-[80%] py-3 px-2">
             <div className="bg-[#f1f1f1] p-2 w-full mb-3 flex items-center justify-between rounded-md">
-              <div className="col1 flex items-center">
-                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full">
+              <div className="col1 flex items-center itemViewActions">
+                <Button className={`!w-[35px] !h-[35px] !min-w-[35px] !rounded-full ${itemView === "grid" && 'active'}`} onClick={()=>setItemView('grid')}>
                   <IoGrid className="!text-[rgba(0,0,0,0.7)] text-[14px]" />
                 </Button>
-                <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full">
+                <Button className={`!w-[35px] !h-[35px] !min-w-[35px] !rounded-full ${itemView === "list" && 'active'}`} onClick={()=>setItemView('list')}>
                   <FaList className="!text-[rgba(0,0,0,0.7)] text-[14px]" />
                 </Button>
                 <span className="text-[14px] hidden sm:block md:block lg:block font-[500] pl-3 text-[rgba(0,0,0,0.7)]">
@@ -70,17 +73,38 @@ const ProductListing = () => {
               </div>
             </div>
 
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
-              <ProductItem />
+            <div className={`grid ${itemView==='grid' ? 'grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5' : 'grid-cols-1'} gap-4`}>
+              {
+                itemView === "grid" ? 
+               <>
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                  <ProductItem />
+                </> 
+                :
+                <>
+                  <ProductItemListView/>
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                  <ProductItemListView />
+                </>
+              }
+            </div>
+            <div className="flex items-center justify-center mt-10">
+              <Pagination count={10} showFirstButton showLastButton />
             </div>
           </div>
         </div>
